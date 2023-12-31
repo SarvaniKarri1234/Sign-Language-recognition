072df6083710d513ffd1825b121247a7c12c6982
SarvaniKarri1234 <155243027+SarvaniKarri1234@users.noreply.github.com>
2023-12-31 16:01:23 +0530

    Create streamlit_app2.py

---
+++ streamlit_app2.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title('Baldness detector')
+st.header('Testing streamlit')
+st.text('Bald?')
+st.text_input('first name')
+st.text_input('last name')
+st.text_input('stress level')
+st.text_input('pressure level')
