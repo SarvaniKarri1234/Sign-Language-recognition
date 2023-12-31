eafa76a5d6f434f3ebc3fed3cf4ce3b30ad24f58
SarvaniKarri1234 <155243027+SarvaniKarri1234@users.noreply.github.com>
2023-12-31 16:04:13 +0530

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -1,8 +1,7 @@
 import streamlit as st
-st.title('Baldness detector')
+st.title('sign language recognition')
 st.header('Testing streamlit')
-st.text('Bald?')
+st.text('recognised the sign?')
 st.text_input('first name')
 st.text_input('last name')
-st.text_input('stress level')
-st.text_input('pressure level')
+st.text_input('type of sign')
