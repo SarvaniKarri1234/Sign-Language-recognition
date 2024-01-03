ac99bd7b37aee804eb1fff1c9d7b566f33d91378
SarvaniKarri1234 <155243027+SarvaniKarri1234@users.noreply.github.com>
2024-01-03 18:45:57 +0530

    Delete streamlit_app2.py

--- streamlit_app2.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-st.title('sign language recognition')
-st.header('Testing streamlit')
-st.text('recognised the sign?')
-st.text_input('first name')
-st.text_input('last name')
-st.text_input('type of sign')
